09cedfb7665ef870748a2b0464139df9002f1a4d
dhjimport <44951441+dhjimport@users.noreply.github.com>
2018-11-11 19:55:05 +0000

    1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+        $(document).ready(function (){
+            $("#click").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#div1").offset().top
+                }, 2000);
+            });
+        });
